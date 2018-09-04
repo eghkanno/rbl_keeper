d9baf75943733c985dc584685ebcbba19b892a55
eghkanno <haruto.kanno@e-grant.net>
2018-09-04 14:04:13 +0900

    datetime conparison

--- rbl.py
+++ rbl.py
@@ -1,9 +1,10 @@
 # coding: utf-8
 
 from time import sleep
-from os import path,remove
+from os import path, remove
 import yaml
 import requests as rq
+from datetime import datetime
 
 CONFIG_FILENAME = "config.yml"
 config = yaml.load(open(CONFIG_FILENAME))
@@ -13,11 +14,15 @@ SLEEP_TIME = float(config["sleep_time"])
 CALLCENTER_URL = config["url"]
 CALLCENTER_PORT = config["port"]
 
+# initialize
 if path.exists("stop"): remove("stop")
+t_ref = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 while not path.exists("stop"):
     responce = rq.get(CALLCENTER_URL + ":" + CALLCENTER_PORT)
     latest_call = responce.json()["latest_call"]
+    if latest_call > t_ref:
+        t_ref = latest_call
     sleep(SLEEP_TIME)
 
 remove("stop")
