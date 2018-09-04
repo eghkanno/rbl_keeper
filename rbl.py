51202b9b13ee070275fb66e250662b8bf6810742
eghkanno <haruto.kanno@e-grant.net>
2018-09-04 13:45:45 +0900

    manual stop

--- rbl.py
+++ rbl.py
@@ -1,1 +1,14 @@
 # coding: utf-8
+
+from time import sleep
+from os import path,remove
+
+SLEEP_TIME=10
+
+if path.exists("stop"): remove("stop")
+
+while not path.exists("stop"):
+    sleep(SLEEP_TIME)
+
+remove("stop")
+print("RBL stopped.")
