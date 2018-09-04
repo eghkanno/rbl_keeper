8d3ca923596912b32bc7625d8e3313f3fcd10f37
eghkanno <haruto.kanno@e-grant.net>
2018-09-04 14:01:21 +0900

    import requests

--- rbl.py
+++ rbl.py
@@ -3,15 +3,21 @@
 from time import sleep
 from os import path,remove
 import yaml
+import requests as rq
 
 CONFIG_FILENAME = "config.yml"
 config = yaml.load(open(CONFIG_FILENAME))
 
-SLEEP_TIME=10
+# polling settings
+SLEEP_TIME = float(config["sleep_time"])
+CALLCENTER_URL = config["url"]
+CALLCENTER_PORT = config["port"]
 
 if path.exists("stop"): remove("stop")
 
 while not path.exists("stop"):
+    responce = rq.get(CALLCENTER_URL + ":" + CALLCENTER_PORT)
+    latest_call = responce.json()["latest_call"]
     sleep(SLEEP_TIME)
 
 remove("stop")
